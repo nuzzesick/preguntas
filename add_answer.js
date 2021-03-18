835b6ff8350381fc36ca1c0459fb1d0e8b00164b
nuzzesick <mati280341@gmail.com>
2021-03-18 16:32:36 -0300

    Juego

--- add_answer.js
+++ add_answer.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const db = require("./db");
 
+const question_id = process.argv[2];
+const n = process.argv[3];
+const label = process.argv[4];
+const is_correct = process.argv[5];
 
 if (process.argv.length !== 6) {
   console.error('Usage: add_answer [question_id] [n] [label] [is_correct]')
@@ -10,7 +14,7 @@ if (process.argv.length !== 6) {
 
 db.query(
   `
-    INSERT INTO questions (question_id, n, label, is_correct) VALUES ()
+    INSERT INTO answers (question_id, n, label, is_correct) VALUES ('${question_id}', '${n}', '${label}', '${is_correct}')
   `
 )
   .then(() => {
--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-const fs = require("fs");
 const db = require("./db");
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-/*
-node index.js [question_id]
+const id = process.argv[2];
 
-¿Que año gano messi el mundial?
-1: 2018
-2: 1998
-3: Nunca
+if (process.argv.length !== 3) {
+  console.error('Usage: [question_id]')
+  process.exit(1);
+}
 
-Rta? 1
+const getAnswers = async (question) => {
+  try {
+    let answers = await db.query(
+      `
+        SELECT * FROM answers WHERE question_id = ${id};
+      `
+    )
+    rl.question(
+      ` ${question}
+        ${answers.rows.map((e) => 
+          `
+            ${e.n}- ${e.label}
+          `
+        ).join('')}
+        Ingrese un número: `
+      , (userAnswer) => {
+      userAnswer = Number(userAnswer);
+      let correct = answers.rows.filter((e) => e.is_correct === true);
+      correct = correct[0].n;
+      userAnswer === correct ? console.log("Respuesta correcta!") : console.log("Respuesta incorrecta!")
+      rl.close();
+    });
+    db.end();
+  } catch (error) {
+    console.error('el error es: ', error);
+  }
+}
 
-Perdiste!
-(Ganaste!)
- */
+const getQuestion = async () => {
+  try {
+    let question = await db.query(
+      `
+        SELECT * FROM questions WHERE id = ${id};
+      `
+    )
+    question = await question.rows[0].label;
+    getAnswers(question);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getQuestion();
