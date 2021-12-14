6869bfb777214327b3009dbc9275f4d1e9101b8f
nuzzesick <mati280341@gmail.com>
2021-12-14 19:39:15 -0300

    Added 25 questions, 75 answers and 5 topics

--- questions.js
+++ questions.js
@@ -1,9 +1,10 @@
 // no agregar ids, que los genere la db
 // generar el n del answer como el índice + 1
 module.exports = [
+	// PREGUNTAS DE DEPORTES
 	{
 		label: '¿Quién gano más balones de oro?',
-		topic: 'deportes'
+		topic: 'deportes',
 		answers: [
 			{
 				label: 'Messi',
@@ -18,5 +19,441 @@ module.exports = [
 				is_correct: false	
 			}
 		]
-	}
+	},
+	{
+		label: '¿Quién ganó el último mundial de fútbol?',
+		topic: 'deportes',
+		answers: [
+			{
+				label: 'Argentina',
+				is_correct: false	
+			},
+			{
+				label: 'Francia',
+				is_correct: true	
+			},
+			{
+				label: 'Holanda',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿Cuántos balones de oro tiene Messi?',
+		topic: 'deportes',
+		answers: [
+			{
+				label: '5',
+				is_correct: false	
+			},
+			{
+				label: '6',
+				is_correct: false	
+			},
+			{
+				label: '7',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿Quién es el actual campeón del fútbol argentino?',
+		topic: 'deportes',
+		answers: [
+			{
+				label: 'Boca',
+				is_correct: false	
+			},
+			{
+				label: 'River',
+				is_correct: true	
+			},
+			{
+				label: 'Independiente',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año murió Diego Armando Maradona?',
+		topic: 'deportes',
+		answers: [
+			{
+				label: '2020',
+				is_correct: true	
+			},
+			{
+				label: '2021',
+				is_correct: false	
+			},
+			{
+				label: '2019',
+				is_correct: false	
+			}
+		]
+	},
+	// PREGUNTAS DE POLITICA
+	{
+		label: '¿Quién es el actual presidente de la República Argentina?',
+		topic: 'politica',
+		answers: [
+			{
+				label: 'CFK',
+				is_correct: false	
+			},
+			{
+				label: 'Alberto Fernández',
+				is_correct: true	
+			},
+			{
+				label: 'Mauricio Macri',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿Quién es el actual ministro de economía de la República Argentina?',
+		topic: 'politica',
+		answers: [
+			{
+				label: 'Martín Guzmán',
+				is_correct: true	
+			},
+			{
+				label: 'Axel Kicillof',
+				is_correct: false	
+			},
+			{
+				label: 'Nicolás Dujovne',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año Argentina volvió a la democracia?',
+		topic: 'politica',
+		answers: [
+			{
+				label: '1981',
+				is_correct: false	
+			},
+			{
+				label: '1982',
+				is_correct: false	
+			},
+			{
+				label: '1983',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿Quién fue el primer presidente democrático luego de la dictadura en Argentina?',
+		topic: 'politica',
+		answers: [
+			{
+				label: 'Carlos Ménem',
+				is_correct: false	
+			},
+			{
+				label: 'Raúl Alfonsín',
+				is_correct: true	
+			},
+			{
+				label: 'Nestor Kirchner',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año serán las eleccions presidenciales en Argentina?',
+		topic: 'politica',
+		answers: [
+			{
+				label: '2024',
+				is_correct: false	
+			},
+			{
+				label: '2022',
+				is_correct: false	
+			},
+			{
+				label: '2023',
+				is_correct: true	
+			}
+		]
+	},
+	// HISTORIA
+	{
+		label: '¿En qué año finalizó la Segunda Guerra Mundial?',
+		topic: 'historia',
+		answers: [
+			{
+				label: '1943',
+				is_correct: false	
+			},
+			{
+				label: '1944',
+				is_correct: false	
+			},
+			{
+				label: '1945',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿En qué año comenzó la Guerra Fría?',
+		topic: 'historia',
+		answers: [
+			{
+				label: '1947',
+				is_correct: true	
+			},
+			{
+				label: '1946',
+				is_correct: false	
+			},
+			{
+				label: '1945',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año comenzó la Guerra de Vietnam?',
+		topic: 'historia',
+		answers: [
+			{
+				label: '1952',
+				is_correct: false	
+			},
+			{
+				label: '1953',
+				is_correct: false	
+			},
+			{
+				label: '1955',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿Cuándo se desencadenó la Revolución Rusa?',
+		topic: 'historia',
+		answers: [
+			{
+				label: '1917',
+				is_correct: true	
+			},
+			{
+				label: '1920',
+				is_correct: false	
+			},
+			{
+				label: '1923',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año se descubrió América?',
+		topic: 'historia',
+		answers: [
+			{
+				label: '1492',
+				is_correct: true	
+			},
+			{
+				label: '1592',
+				is_correct: false	
+			},
+			{
+				label: '1498',
+				is_correct: false	
+			}
+		]
+	},
+	// TECNOLOGÍA
+	{
+		label: '¿En qué año se inició la compañia Apple, Inc.?',
+		topic: 'tecnologia',
+		answers: [
+			{
+				label: '1973',
+				is_correct: false	
+			},
+			{
+				label: '1976',
+				is_correct: true	
+			},
+			{
+				label: '1978',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año se inició la compañia Facebook?',
+		topic: 'tecnologia',
+		answers: [
+			{
+				label: '2002',
+				is_correct: false	
+			},
+			{
+				label: '2003',
+				is_correct: false	
+			},
+			{
+				label: '2004',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿En qué año Jack Dorsey abandonó Twitter?',
+		topic: 'tecnologia',
+		answers: [
+			{
+				label: '2019',
+				is_correct: false	
+			},
+			{
+				label: '2021',
+				is_correct: true	
+			},
+			{
+				label: '2018',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿Quién fue el cofundador de Apple y amigo de Steve Jobs?',
+		topic: 'tecnologia',
+		answers: [
+			{
+				label: 'Steve Wozniak',
+				is_correct: true	
+			},
+			{
+				label: 'Paul Allen',
+				is_correct: false	
+			},
+			{
+				label: 'Mark Zuckerberg',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿Qué famoso juego fue creado para que las personas aprendan a usar el mouse?',
+		topic: 'tecnologia',
+		answers: [
+			{
+				label: 'Counter Strike',
+				is_correct: false	
+			},
+			{
+				label: 'Buscaminas',
+				is_correct: true	
+			},
+			{
+				label: 'League of Legends',
+				is_correct: false	
+			}
+		]
+	},
+	// Musica
+	{
+		label: '¿En qué año falleció Kurt Cobain?',
+		topic: 'musica',
+		answers: [
+			{
+				label: '1995',
+				is_correct: false	
+			},
+			{
+				label: '1994',
+				is_correct: true	
+			},
+			{
+				label: '1990',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿Quién lideró el grupo de rock Sumo?',
+		topic: 'musica',
+		answers: [
+			{
+				label: 'Federico Moura',
+				is_correct: false	
+			},
+			{
+				label: 'Miguel Abuelo',
+				is_correct: false	
+			},
+			{
+				label: 'Luca Prodan',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿Cuál es la duración exacta de la canción Bohemian Rhapsody compuesta por Queen?',
+		topic: 'musica',
+		answers: [
+			{
+				label: '05:55',
+				is_correct: true	
+			},
+			{
+				label: '06:00',
+				is_correct: false	
+			},
+			{
+				label: '06:30',
+				is_correct: false	
+			}
+		]
+	},
+	{
+		label: '¿En qué año se publicó el famoso disco Nevermind de Nirvana?',
+		topic: 'musica',
+		answers: [
+			{
+				label: '1989',
+				is_correct: false	
+			},
+			{
+				label: '1990',
+				is_correct: false	
+			},
+			{
+				label: '1991',
+				is_correct: true	
+			}
+		]
+	},
+	{
+		label: '¿En qué año se publicó el famoso disco Nevermind de Nirvana?',
+		topic: 'musica',
+		answers: [
+			{
+				label: '1989',
+				is_correct: false	
+			},
+			{
+				label: '1990',
+				is_correct: false	
+			},
+			{
+				label: '1991',
+				is_correct: true	
+			}
+		]
+	},
 ]
