25e4020b30ba4633b014a9bf9c1ce443b5df8740
nuzzesick <mati280341@gmail.com>
2021-12-16 16:23:00 -0300

    script

--- questions.js
+++ questions.js
@@ -3,455 +3,455 @@
 module.exports = [
 	// PREGUNTAS DE DEPORTES
 	{
-		label: '¿Quién gano más balones de oro?',
-		topic: 'deportes',
+		label: "¿Quién gano más balones de oro?",
+		topic: "deportes",
 		answers: [
 			{
-				label: 'Messi',
+				label: "Messi",
 				is_correct: true	
 			},
 			{
-				label: 'Ronaldo',
+				label: "Ronaldo",
 				is_correct: false	
 			},
 			{
-				label: 'Kaka',
+				label: "Kaka",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Quién ganó el último mundial de fútbol?',
-		topic: 'deportes',
+		label: "¿Quién ganó el último mundial de fútbol?",
+		topic: "deportes",
 		answers: [
 			{
-				label: 'Argentina',
+				label: "Argentina",
 				is_correct: false	
 			},
 			{
-				label: 'Francia',
+				label: "Francia",
 				is_correct: true	
 			},
 			{
-				label: 'Holanda',
+				label: "Holanda",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Cuántos balones de oro tiene Messi?',
-		topic: 'deportes',
+		label: "¿Cuántos balones de oro tiene Messi?",
+		topic: "deportes",
 		answers: [
 			{
-				label: '5',
+				label: "5",
 				is_correct: false	
 			},
 			{
-				label: '6',
+				label: "6",
 				is_correct: false	
 			},
 			{
-				label: '7',
+				label: "7",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿Quién es el actual campeón del fútbol argentino?',
-		topic: 'deportes',
+		label: "¿Quién es el actual campeón del fútbol argentino?",
+		topic: "deportes",
 		answers: [
 			{
-				label: 'Boca',
+				label: "Boca",
 				is_correct: false	
 			},
 			{
-				label: 'River',
+				label: "River",
 				is_correct: true	
 			},
 			{
-				label: 'Independiente',
+				label: "Independiente",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año murió Diego Armando Maradona?',
-		topic: 'deportes',
+		label: "¿En qué año murió Diego Armando Maradona?",
+		topic: "deportes",
 		answers: [
 			{
-				label: '2020',
+				label: "2020",
 				is_correct: true	
 			},
 			{
-				label: '2021',
+				label: "2021",
 				is_correct: false	
 			},
 			{
-				label: '2019',
+				label: "2019",
 				is_correct: false	
 			}
 		]
 	},
 	// PREGUNTAS DE POLITICA
 	{
-		label: '¿Quién es el actual presidente de la República Argentina?',
-		topic: 'politica',
+		label: "¿Quién es el actual presidente de la República Argentina?",
+		topic: "politica",
 		answers: [
 			{
-				label: 'CFK',
+				label: "CFK",
 				is_correct: false	
 			},
 			{
-				label: 'Alberto Fernández',
+				label: "Alberto Fernández",
 				is_correct: true	
 			},
 			{
-				label: 'Mauricio Macri',
+				label: "Mauricio Macri",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Quién es el actual ministro de economía de la República Argentina?',
-		topic: 'politica',
+		label: "¿Quién es el actual ministro de economía de la República Argentina?",
+		topic: "politica",
 		answers: [
 			{
-				label: 'Martín Guzmán',
+				label: "Martín Guzmán",
 				is_correct: true	
 			},
 			{
-				label: 'Axel Kicillof',
+				label: "Axel Kicillof",
 				is_correct: false	
 			},
 			{
-				label: 'Nicolás Dujovne',
+				label: "Nicolás Dujovne",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año Argentina volvió a la democracia?',
-		topic: 'politica',
+		label: "¿En qué año Argentina volvió a la democracia?",
+		topic: "politica",
 		answers: [
 			{
-				label: '1981',
+				label: "1981",
 				is_correct: false	
 			},
 			{
-				label: '1982',
+				label: "1982",
 				is_correct: false	
 			},
 			{
-				label: '1983',
+				label: "1983",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿Quién fue el primer presidente democrático luego de la dictadura en Argentina?',
-		topic: 'politica',
+		label: "¿Quién fue el primer presidente democrático luego de la dictadura en Argentina?",
+		topic: "politica",
 		answers: [
 			{
-				label: 'Carlos Ménem',
+				label: "Carlos Ménem",
 				is_correct: false	
 			},
 			{
-				label: 'Raúl Alfonsín',
+				label: "Raúl Alfonsín",
 				is_correct: true	
 			},
 			{
-				label: 'Nestor Kirchner',
+				label: "Nestor Kirchner",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año serán las eleccions presidenciales en Argentina?',
-		topic: 'politica',
+		label: "¿En qué año serán las eleccions presidenciales en Argentina?",
+		topic: "politica",
 		answers: [
 			{
-				label: '2024',
+				label: "2024",
 				is_correct: false	
 			},
 			{
-				label: '2022',
+				label: "2022",
 				is_correct: false	
 			},
 			{
-				label: '2023',
+				label: "2023",
 				is_correct: true	
 			}
 		]
 	},
 	// HISTORIA
 	{
-		label: '¿En qué año finalizó la Segunda Guerra Mundial?',
-		topic: 'historia',
+		label: "¿En qué año finalizó la Segunda Guerra Mundial?",
+		topic: "historia",
 		answers: [
 			{
-				label: '1943',
+				label: "1943",
 				is_correct: false	
 			},
 			{
-				label: '1944',
+				label: "1944",
 				is_correct: false	
 			},
 			{
-				label: '1945',
+				label: "1945",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿En qué año comenzó la Guerra Fría?',
-		topic: 'historia',
+		label: "¿En qué año comenzó la Guerra Fría?",
+		topic: "historia",
 		answers: [
 			{
-				label: '1947',
+				label: "1947",
 				is_correct: true	
 			},
 			{
-				label: '1946',
+				label: "1946",
 				is_correct: false	
 			},
 			{
-				label: '1945',
+				label: "1945",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año comenzó la Guerra de Vietnam?',
-		topic: 'historia',
+		label: "¿En qué año comenzó la Guerra de Vietnam?",
+		topic: "historia",
 		answers: [
 			{
-				label: '1952',
+				label: "1952",
 				is_correct: false	
 			},
 			{
-				label: '1953',
+				label: "1953",
 				is_correct: false	
 			},
 			{
-				label: '1955',
+				label: "1955",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿Cuándo se desencadenó la Revolución Rusa?',
-		topic: 'historia',
+		label: "¿Cuándo se desencadenó la Revolución Rusa?",
+		topic: "historia",
 		answers: [
 			{
-				label: '1917',
+				label: "1917",
 				is_correct: true	
 			},
 			{
-				label: '1920',
+				label: "1920",
 				is_correct: false	
 			},
 			{
-				label: '1923',
+				label: "1923",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año se descubrió América?',
-		topic: 'historia',
+		label: "¿En qué año se descubrió América?",
+		topic: "historia",
 		answers: [
 			{
-				label: '1492',
+				label: "1492",
 				is_correct: true	
 			},
 			{
-				label: '1592',
+				label: "1592",
 				is_correct: false	
 			},
 			{
-				label: '1498',
+				label: "1498",
 				is_correct: false	
 			}
 		]
 	},
 	// TECNOLOGÍA
 	{
-		label: '¿En qué año se inició la compañia Apple, Inc.?',
-		topic: 'tecnologia',
+		label: "¿En qué año se inició la compañia Apple, Inc.?",
+		topic: "tecnologia",
 		answers: [
 			{
-				label: '1973',
+				label: "1973",
 				is_correct: false	
 			},
 			{
-				label: '1976',
+				label: "1976",
 				is_correct: true	
 			},
 			{
-				label: '1978',
+				label: "1978",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año se inició la compañia Facebook?',
-		topic: 'tecnologia',
+		label: "¿En qué año se inició la compañia Facebook?",
+		topic: "tecnologia",
 		answers: [
 			{
-				label: '2002',
+				label: "2002",
 				is_correct: false	
 			},
 			{
-				label: '2003',
+				label: "2003",
 				is_correct: false	
 			},
 			{
-				label: '2004',
+				label: "2004",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿En qué año Jack Dorsey abandonó Twitter?',
-		topic: 'tecnologia',
+		label: "¿En qué año Jack Dorsey abandonó Twitter?",
+		topic: "tecnologia",
 		answers: [
 			{
-				label: '2019',
+				label: "2019",
 				is_correct: false	
 			},
 			{
-				label: '2021',
+				label: "2021",
 				is_correct: true	
 			},
 			{
-				label: '2018',
+				label: "2018",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Quién fue el cofundador de Apple y amigo de Steve Jobs?',
-		topic: 'tecnologia',
+		label: "¿Quién fue el cofundador de Apple y amigo de Steve Jobs?",
+		topic: "tecnologia",
 		answers: [
 			{
-				label: 'Steve Wozniak',
+				label: "Steve Wozniak",
 				is_correct: true	
 			},
 			{
-				label: 'Paul Allen',
+				label: "Paul Allen",
 				is_correct: false	
 			},
 			{
-				label: 'Mark Zuckerberg',
+				label: "Mark Zuckerberg",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Qué famoso juego fue creado para que las personas aprendan a usar el mouse?',
-		topic: 'tecnologia',
+		label: "¿Qué famoso juego fue creado para que las personas aprendan a usar el mouse?",
+		topic: "tecnologia",
 		answers: [
 			{
-				label: 'Counter Strike',
+				label: "Counter Strike",
 				is_correct: false	
 			},
 			{
-				label: 'Buscaminas',
+				label: "Buscaminas",
 				is_correct: true	
 			},
 			{
-				label: 'League of Legends',
+				label: "League of Legends",
 				is_correct: false	
 			}
 		]
 	},
 	// Musica
 	{
-		label: '¿En qué año falleció Kurt Cobain?',
-		topic: 'musica',
+		label: "¿En qué año falleció Kurt Cobain?",
+		topic: "musica",
 		answers: [
 			{
-				label: '1995',
+				label: "1995",
 				is_correct: false	
 			},
 			{
-				label: '1994',
+				label: "1994",
 				is_correct: true	
 			},
 			{
-				label: '1990',
+				label: "1990",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿Quién lideró el grupo de rock Sumo?',
-		topic: 'musica',
+		label: "¿Quién lideró el grupo de rock Sumo?",
+		topic: "musica",
 		answers: [
 			{
-				label: 'Federico Moura',
+				label: "Federico Moura",
 				is_correct: false	
 			},
 			{
-				label: 'Miguel Abuelo',
+				label: "Miguel Abuelo",
 				is_correct: false	
 			},
 			{
-				label: 'Luca Prodan',
+				label: "Luca Prodan",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿Cuál es la duración exacta de la canción Bohemian Rhapsody compuesta por Queen?',
-		topic: 'musica',
+		label: "¿Cuál es la duración exacta de la canción Bohemian Rhapsody compuesta por Queen?",
+		topic: "musica",
 		answers: [
 			{
-				label: '05:55',
+				label: "05:55",
 				is_correct: true	
 			},
 			{
-				label: '06:00',
+				label: "06:00",
 				is_correct: false	
 			},
 			{
-				label: '06:30',
+				label: "06:30",
 				is_correct: false	
 			}
 		]
 	},
 	{
-		label: '¿En qué año se publicó el famoso disco Nevermind de Nirvana?',
-		topic: 'musica',
+		label: "¿En qué año se publicó el famoso disco Nevermind de Nirvana?",
+		topic: "musica",
 		answers: [
 			{
-				label: '1989',
+				label: "1989",
 				is_correct: false	
 			},
 			{
-				label: '1990',
+				label: "1990",
 				is_correct: false	
 			},
 			{
-				label: '1991',
+				label: "1991",
 				is_correct: true	
 			}
 		]
 	},
 	{
-		label: '¿En qué año se publicó el famoso disco Nevermind de Nirvana?',
-		topic: 'musica',
+		label: "¿En qué año se publicó el famoso disco Nevermind de Nirvana?",
+		topic: "musica",
 		answers: [
 			{
-				label: '1989',
+				label: "1989",
 				is_correct: false	
 			},
 			{
-				label: '1990',
+				label: "1990",
 				is_correct: false	
 			},
 			{
-				label: '1991',
+				label: "1991",
 				is_correct: true	
 			}
 		]
--- script.js
+++ script.js
@@ -12,3 +12,71 @@ node script.js
 
 
 */
+const db = require("./db");
+const questions = require('./questions');
+
+const createDatabaseSchema = async () => {
+  try {
+    await db.query(
+      `CREATE TABLE questions (id SERIAL PRIMARY KEY, label TEXT NOT NULL, topic TEXT NOT NULL)`
+    );
+    await db.query(
+      `CREATE TABLE answers (
+        id SERIAL PRIMARY KEY,
+        question_id INTEGER,
+        label TEXT NOT NULL,
+        is_correct BOOL DEFAULT false
+    )`);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const addQuestions = async () => {
+  try {
+    for await (const question of questions) {
+      await db.query(
+        `
+          INSERT INTO questions (label, topic) VALUES ('${question.label}', '${question.topic}')
+        `
+      )
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const addAnswers = async () => {
+  try {
+    for await (const question of questions) {
+      const response = await db.query(`SELECT id FROM questions WHERE label = '${question.label}'`);
+      const questionId = await response.rows[0].id;
+      for await (const answer of questions.map((el) => el.answers)) {
+        console.log(questionId)
+        await db.query(
+          `INSERT INTO answers (question_id, label, is_correct) VALUES ('${questionId}', '${answer.label}', ${answer.is_correct});`
+        )
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const addDataToDatabase = async () => {
+  try {
+    await addQuestions();
+    await addAnswers();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+createDatabaseSchema()
+  .then(async () => {
+    const data = await addDataToDatabase();
+    process.exit(1);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
